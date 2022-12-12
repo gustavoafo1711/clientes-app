803305c540b2ea7a38e57497aa0cb085d7e571aa
gustavo <gustavoafo@hotmail.com>
2022-12-12 17:27:21 -0300

    Navegando nas páginas através de Routerlinks

--- src/app/clientes/clientes-routing.module.ts
+++ src/app/clientes/clientes-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ClientesFormComponent } from './clientes-form/clientes-form.component';
+
+const routes: Routes = [
+  { path: 'clientes-form', component: ClientesFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
