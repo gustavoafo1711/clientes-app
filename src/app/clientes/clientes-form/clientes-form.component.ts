e4de8d224a4c356c92940298c370775777c9c0dc
gustavo <gustavoafo@hotmail.com>
2022-12-13 10:47:39 -0300

    Desenvolvendo o Formulário de Clientes

--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+import { Cliente } from '../cliente';
+
 @Component({
   selector: 'app-clientes-form',
   templateUrl: './clientes-form.component.html',
   styleUrls: ['./clientes-form.component.css']
 })
 export class ClientesFormComponent {
-
+  cliente: Cliente;
+  
 }
