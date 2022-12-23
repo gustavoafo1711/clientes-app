ea1ae1e20582d6027b5a0483f625eeb649ad6a03
gustavo <gustavoafo@hotmail.com>
2022-12-23 16:32:03 -0300

    Concluindo a deleção de clientes

--- src/app/clientes/clientes-lista/clientes-lista.component.ts
+++ src/app/clientes/clientes-lista/clientes-lista.component.ts
@@ -13,6 +13,8 @@ export class ClientesListaComponent {
 
   clientes: Cliente[] = [];
   clienteSelecionado: Cliente;
+  mensagemSucesso: string;
+  mensagemErro: string;
 
   constructor( private service: ClientesService,
                private router: Router ){ }
@@ -28,6 +30,15 @@ export class ClientesListaComponent {
 
   preparaDelecao(cliente: Cliente){
     this.clienteSelecionado = cliente;
+  }
+
+  deletarCliente(){
+    this.service.deletar(this.clienteSelecionado)
+                .subscribe( response => {
+                            this.mensagemSucesso = 'Cliente deletado com sucesso.'
+                            this.ngOnInit();
+                          },
+                            erro => this.mensagemErro = 'Ocorreu um erro ao deletar o cliente.')
   }
 
 }
