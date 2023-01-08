66982cc76f80cd6ed01ae997df0faaf995b816e1
gustavo <gustavoafo@hotmail.com>
2023-01-08 19:29:27 -0300

    Serviços Prestados e variáveis de ambiente utilizanod Environment

---
+++ src/app/servico-prestado.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ServicoPrestado } from './servico-prestado/servicoPrestado';
+
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServicoPrestadoService {
+
+  apiURL: string = environment.apiURLBase + '/api/servicos-prestados'
+
+  constructor(private http: HttpClient) { }
+
+  salvar(servicoPrestado: ServicoPrestado): Observable<ServicoPrestado>{
+    return this.http.post<ServicoPrestado>('', servicoPrestado);
+  }
+}
