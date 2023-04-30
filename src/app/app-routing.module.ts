b7cfdab7948fd9d93fe80c0560f5f341b742f4f2
gustavo <gustavoafo@hotmail.com>
2023-04-30 17:59:29 -0300

    Criação do Formulário de Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import{ HomeComponent } from './home/home.component'
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
+  { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent }
+
 ];
 
 @NgModule({
