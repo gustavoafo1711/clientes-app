492f96b19f9c056b4d881faa7bae0fd91ee4f9d5
gustavo <gustavoafo@hotmail.com>
2023-05-10 17:11:42 -0300

    Finalizando o Cadastro de Usuario

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -13,9 +13,9 @@ export class LoginComponent {
 
   username: string;
   password: string;
-  loginError: boolean;
   cadastrando: boolean;
   mensagemSucesso: string;
+  errors: String[];
 
   constructor( private router: Router,
                private authService: AuthService ) { }
@@ -41,9 +41,8 @@ export class LoginComponent {
         .salvar(usuario)
         .subscribe({
           next: response => {this.mensagemSucesso = 'Cadastro realizado com sucesso! Efetue o login.'
-                             this.loginError = false
         },
-          error: errorResponse => {this.loginError = true,
+          error: errorResponse => {this.errors = errorResponse.error.errors,
                                    this.mensagemSucesso = null}
         })
   }
